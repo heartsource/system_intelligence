a736791e544df93d43eb66e48846077010ea1f58
sudhir4hsrc <167919960+sudhir4hsrc@users.noreply.github.com>
2024-04-24 20:48:55 -0400

    removed old files
    Remove old code

--- install.sh
+++
@@ -1,6 +0,0 @@
-docker build -t myil/productsupportbackendapi:1.2 .
-docker push myil/productsupportbackendapi:1.2
-kubectl apply -f ./k8/deployment.yaml
-kubectl apply -f ./k8/service.yaml
-minikube service productsupportbackendapp-service --url
-
