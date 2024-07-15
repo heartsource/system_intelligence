29449f30b6a046c8171e5a7d17bff518f0106e3a
poornagollakoti <chandra.gollakoti@gmail.com>
2024-07-15 19:00:58 +0530

    updated packages to support async code

--- backend/app/config/mongodb_config.py
+++ backend/app/config/mongodb_config.py
@@ -3,20 +3,17 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi import  HTTPException
 from dotenv import load_dotenv
 import utils.constants.error_constants as ERROR_CONSTANTS
+from pymongo.server_api import ServerApi
 
 load_dotenv()
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://si-admin:SI-admin1234@si-cluster.abw4huo.mongodb.net/?retryWrites=true&w=majority&appName=si-cluster")
-MONGO_DB_NAME = os.getenv("MONGO_DB", "system_intelligence_db")
-mongo_client = AsyncIOMotorClient(MONGO_URI)
-mongo_db = mongo_client.get_database(MONGO_DB_NAME)
 
 class MongoConfig:
     def __init__(self):
         try:
-            self.host = os.getenv('MONGO_HOST', 'mongodb+srv://si-admin:SI-admin1234@si-cluster.abw4huo.mongodb.net/?retryWrites=true&w=majority&appName=si-cluster')
+            self.host = os.getenv('MONGO_URI', 'mongodb+srv://si-admin:SI-admin1234@si-cluster.abw4huo.mongodb.net/?retryWrites=true&w=majority&appName=si-cluster')
             self.port = int(os.getenv('MONGO_PORT', 27017))
             self.db_name = os.getenv('MONGO_DB_NAME', 'system_intelligence_db')
-            self.client = AsyncIOMotorClient(self.host, self.port)
+            self.client = AsyncIOMotorClient(self.host, server_api=ServerApi('1'))
             self.db = self.client[self.db_name]
         except Exception as e:
              print(f"Error connecting to MongoDB: {e}")
