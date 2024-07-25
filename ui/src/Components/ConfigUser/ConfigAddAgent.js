64168a5ccb9d51314da7a36a222bbd0fb653ac96
poornagollakoti <chandra.gollakoti@gmail.com>
2024-07-25 19:29:34 +0530

    created docker compose to support env variables

--- ui/src/Components/ConfigUser/ConfigAddAgent.js
+++ ui/src/Components/ConfigUser/ConfigAddAgent.js
@@ -32,9 +32,9 @@ const ConfigAddAgent = () => {
 
   const fetchData = async () => {
     try {
-      console.log("REACT_APP_HEARTIE_BE" + process.env)
-      console.log("REACT_APP_HEARTIE_BE" + process.env.REACT_APP_HEARTIE_BE)
-      console.log("HEARTIE_BE" + process.env.HEARTIE_BE)
+      console.log("REACT_APP_HEARTIE_BE: " + process.env)
+      console.log("REACT_APP_HEARTIE_BE: " + process.env.REACT_APP_HEARTIE_BE)
+      console.log("HEARTIE_BE: " + process.env.HEARTIE_BE)
       const response = await axios.get(
         process.env.REACT_APP_HEARTIE_BE + "/get_ai_prompts/"
       );
